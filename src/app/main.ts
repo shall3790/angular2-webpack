be8c09110caefa91e26f21db6e0984ab53cebe97
Stephen Hall <stephen.hall@hobbylobby.com>
2016-07-12 16:00:26 -0500

    added bootstrap css

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,9 @@
 import 'core-js/es6';
 import 'reflect-metadata';
 require('zone.js/dist/zone');
+// css
+require('../../css/bootstrap.css');
+require('../../css/app.css');
 
 
 import { bootstrap }    from '@angular/platform-browser-dynamic';
